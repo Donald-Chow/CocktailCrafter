812b7b00523e6fcc1af651f430eacb83502f101a
Donald Chow <donald7290@gmail.com>
2023-08-05 00:36:49 +0900

    Added random Cocktail on home page from cocktaildbapi

--- cocktail/views.py
+++ cocktail/views.py
@@ -1,3 +1,6 @@
+import requests
+from .cocktaildb import Cocktaildb
+
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -6,7 +9,15 @@ from .models import Alcohol, Recipe, Ingredient, Inventory, Favorite
 
 # Create your views here.
 def home(request):
-    return render(request, 'cocktail/home.html')
+    response = requests.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    if response.status_code == 200:
+        data = response.json()
+        context = {'cocktail': Cocktaildb(data['drinks'][0]).parse()}
+        print(context)
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+    return render(request, 'cocktail/home.html', context)
 
 
 class AlcoholList(ListView):
